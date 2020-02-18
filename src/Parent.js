dd62fbecdab7d21e3f148bb29605f6457d96173b
SwethaRedd <swethareddy.nadipi@gmail.com>
2020-02-17 20:38:51 -0600

    initial commit

---
+++ src/Parent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Parent extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> this is Parent</h1>
+            </div>
+        )
+    }
+}
